05301bdd87a3bc066f271a3bbee7c59b57af8748
Corey Gallagher <gallacor@oregonstate.edu>
2022-11-12 12:05:46 -0500

    update testing file and start work on read functions

--- controller.mjs
+++ controller.mjs
@@ -4,6 +4,17 @@ import {Project} from './models/project.mjs'
 import { Workspace } from './models/workspace.mjs'
 import { workspaceUser } from './models/workspaceUser.mjs'
 import { projectUser } from './models/projectUser.mjs'
+import { sequelize } from './models/db_init.mjs'
+import {JSON, Sequelize} from 'sequelize'
+const Op = Sequelize.Op;
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
 
 // Todo - Refactor into separate controller files in controllers directory. e.g. controllers/project.controller.mjs
 export async function createProject (project_obj){
@@ -13,17 +24,28 @@ export async function createProject (project_obj){
     await Project.create({
         project_name: project_obj.project_name,
         project_owner: project_obj.use_id,
-        work_id: project_obj.work_id
-        }).catch((err)=>{
+        work_id: project_obj.work_id})
+        //get the next project_id
+    let project = await Project.findAll({
+            attributes: [[sequelize.fn('MAX', sequelize.col('project_id')), 'max_id']],
+            raw: true
+        })
+        console.log('--------------------------------------')
+        console.log(JSON.stringify(project))
+        console.log(project[0].max_id)
+        console.log('--------------------------------------')
+        sleep(1000)
+        await projectUser.create({
+        user_user_id: project_obj.use_id,
+        role: "owner",
+        project_project_id: project[0].max_id // need to get the newly created project id here
+            }).catch((err)=>{
+                console.log(err)
+            })
+        .catch((err)=>{
             console.log(err)
         })
-   await projectUser.create({
-    user_user_id: project_obj.use_id,
-    role: "owner",
-    project_project_id: 1 // need to get the newly created project id here
-    }).catch((err)=>{
-        console.log(err)
-    })
+
     }
 
 export async function createWorkspace (workspace_obj ){
@@ -130,16 +152,41 @@ export async function updateProject(){
     
 }
 
-export async function readProject(){
+export async function readProjects(workspace_id){
+    await Project.findAll({where:{
+        work_id:  workspace_id
+    }})
+
   
 }
+async function get_user_ids(user_objs){
+    let users_in_workspace = []
+    for(const user of user_objs){
+        console.log(user)
+    users_in_workspace.push(user.user_id)}
+}
+export async function readUsersWorkspace(workspace_id){
+const user_objs = await workspaceUser.findAll({where:{
+                        workspace_workspace_id: workspace_id
+                    }})
 
-export async function readUser(){
-  
+//build list of user_ids in the workspace
+const users_in_workspace = await get_user_ids(user_objs)
+const users = await User.findAll({where:{
+                        user_id:{
+                        [Op.in]: users_in_workspace
+                        } 
+                    }})
 }
 
-export async function readTask(){
-  
+export async function readUsersProject(project_id){
+ 
+}
+
+export async function readTasks(project_id){
+    Task.findAll({where:{
+        proj_id:  project_id
+    }})
 }
 
 export async function readWorkspace(){
