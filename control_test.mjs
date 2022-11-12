3418dcaee8430e23cf978b94433111822574048d
Corey Gallagher <gallacor@oregonstate.edu>
2022-11-11 20:12:11 -0500

    got tasks and projects table both recieiving data correctly

--- control_test.mjs
+++ control_test.mjs
@@ -1,5 +1,23 @@
-import {createTask, createProject, createUser} from './controller.mjs'
+import {createTask, createProject, createUser, createWorkspace} from './controller.mjs'
 
-const user  =  createUser('test_name', 'test_lname', 'testmail')
-const project = createProject('projname', 'projownertest')
-const task = createTask("taskname1","corey","complete", 7,"build a log cavbin")
+const user  =  createUser({first_name: "corey",
+                            last_name: "gallagher",
+                            email: "email@test.com"})
+
+const project = createProject({work_id: 17327327,
+                                project_name: "projectname1",
+                                use_id: 1})
+
+
+const task = createTask({proj_id: 18239129,
+                        use_id: 49289123,
+                        task_name: "taskname1",
+                        task_owner: "corey",
+                        task_status: "complete", 
+                        task_value: 7,
+                        date_ended: 0,
+                        task_descriptions:"build a log cavbin"})
+
+                        
+const workspace = createWorkspace({workspace_name: "taskname1",
+                                    use_id: 1})
