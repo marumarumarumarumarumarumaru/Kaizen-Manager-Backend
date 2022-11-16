68d53cbaedfcd729fe5b7130b5879621eab2da73
gallacor <76980212+gallacor@users.noreply.github.com>
2022-11-16 13:02:48 -0500

    created readTaskLastTwoWeeks function
    work on filtering tasks by date

--- controllers/task_controller.mjs
+++ controllers/task_controller.mjs
@@ -33,7 +33,7 @@ export async function deleteTask (task_id_num) {
 export async function createTask (task_obj){
     await Task.create({
         proj_id: task_obj.proj_id, //FK
-        task_assignee: task_obj.task_assignee, //FK
+        task_assignee: task_obj.task_assignee, 
         task_name: task_obj.task_name,
         task_value: task_obj.task_value,
         task_due_date: task_obj.task_due_date,
@@ -47,18 +47,21 @@ export async function createTask (task_obj){
     return true
 }
 
+// retruns list of task objects from a project that were not marked as done before the previous two weeks
 export async function readTasksLastTwoWeeks(proj_id){
-    let current_time = sequelize.fn('NOW')
-    const tasks_completed = await Task.findAll({where:{
-        [Op.and]:[
-            {proj_id : proj_id},
-            {date_created:{
-                [Op.or]:{
-                    [Op.lt]: current_time,
-                    [Op.gt]: current_time - 14
-                }
-            }}
-        ]
-    }})
-    console.log(tasks_completed)
+    const tasks_in_timeframe = await Task.findAll(
+        {where:{proj_id : proj_id}}
+        ).catch((err)=>{
+            console.log(err)
+        })
+    let tasks = []
+    for(const task of tasks_in_timeframe){
+        let date_ended = task.dataValues.date_ended
+        let cutoff_date = new Date()
+        cutoff_date.setDate(cutoff_date.getDate() - 14)
+        if (date_ended >= cutoff_date){
+            tasks.push(task.dataValues)
+        }
+    }
+    return tasks
 }
